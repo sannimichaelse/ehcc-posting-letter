fff40e1a5eaba488d80698a6835907103a3218d2
Sanni Michael <user@Tomiwa-MacBook-Pro.local>
2019-05-19 14:13:13 +0100

    Added departmental head and directorate head phone numbers

--- js/ehcc.js
+++ js/ehcc.js
@@ -34,7 +34,7 @@ document.getElementById("submitBtn").addEventListener("click", () => {
     localStorage.setItem("loveCircleArea", lovecircle.options[lovecircle.selectedIndex].text);
     localStorage.setItem("loveCircleAddress", loveCircleDetails.address);
     localStorage.setItem("loveCirclePhone", loveCircleDetails.phone);
-    localStorage.setItem("loveCircleLeader", loveCircleDetails.name)
+    localStorage.setItem("loveCircleLeader", loveCircleDetails.name);
 
     window.location.href = "ehcc.html";
 
@@ -149,21 +149,29 @@ const directorateObject = [
     {
         name: "ADMINISTRATION DIRECTORATE",
         head: "Minister Toba Oluwapomile",
+        headOfDirectoratePhone: "08060194260",
         deputy: "Miss Stella Ogundipe",
+        gender: "m",
         departments: [
             {
                 name: "TRAINING DEPARTMENT",
-                head: "Miss Stella Ogundipe",
+                head: "Mrs Stella Ogundipe",
+                gender: "f",
+                headOfDepartmentPhone: "07064272239",
                 deputy: "Mr. Tunde Obisesan "
             },
             {
                 name: "INFORMATION SYSTEMS DEPARTMENT",
                 head: "Mr. Wale Onifade",
+                gender: "m",
+                headOfDepartmentPhone: "08063020238",
                 deputy: "Mr. Tosin Oguntegbe"
             },
             {
                 name: "EDITORIAL DEPARTMENT",
                 head: "Mrs Tomi Fadoju",
+                gender: "f",
+                headOfDepartmentPhone: "08128921465",
                 deputy: "Miss Olanrewaju Adekunle"
             }
         ],
@@ -171,36 +179,50 @@ const directorateObject = [
     {
         name: "MEMBERSHIP DIRECTORATE",
         head: "Min. Doyin Kolawole",
+        headOfDirectoratePhone: "08165275623",
         deputy: "Mr. Ibukun Fagbeyiro",
+        gender: "m",
         departments: [
             {
                 name: "PROTOCOL DEPARTMENT",
                 head: "Mr. Ibukun Fagbeyiro",
+                gender: "m",
+                headOfDepartmentPhone: "08062438953",
                 deputy: "Mr. Gbenga Paul"
             },
             {
                 name: "USHERING DEPARTMENT",
                 head: "Mrs Adedayo Olufemi",
+                gender: "f",
+                headOfDepartmentPhone: "08055384847",
                 deputy: "Mr Rotimi Adedoyin"
             },
             {
                 name: "GREETERS DEPARTMENT",
                 head: "Mrs. Ebunlomo Oguntoye",
+                gender: "f",
+                headOfDepartmentPhone: "08069782421",
                 deputy: "Mr Yinka Koyejo"
             },
             {
                 name: "SECURITY DEPARTMENT",
                 head: "Mr. Tayo Olatayo",
+                gender: "m",
+                headOfDepartmentPhone: "08033157504",
                 deputy: "Mr. Adeolu Lala"
             },
             {
                 name: "ASSIMILATION DEPARTMENT",
                 head: "Mrs. Abimbola Oluwaponmile ",
+                gender: "f",
+                headOfDepartmentPhone: "08138893397",
                 deputy: "Miss Yvonne Asemota"
             },
             {
                 name: "CALL CENTRE DEPARTMENT",
                 head: "Mr Olumide Dickson",
+                gender: "m",
+                headOfDepartmentPhone: "08036461497",
                 deputy: "Miss Mirabel Samuel"
             }
         ],
@@ -208,46 +230,64 @@ const directorateObject = [
     {
         name: "WELFARE DIRECTORATE",
         head: "Min. Ololade Hassan",
+        headOfDirectoratePhone: "07036032729",
         deputy: "Mrs. Torera Jide-Olaleye",
+        gender: "m",
         departments: [
             {
                 name: "CARE DEPARTMENT",
                 head: "Mrs. Wunmi Fagbeyiro",
+                gender: "f",
+                headOfDepartmentPhone: "07068253608",
                 deputy: "Mrs. Doyin Dickson "
             },
             {
                 name: "CHILDREN DEPARTMENT",
                 head: "Mrs. Torera Jide-Olaleye ",
+                gender: "f",
+                headOfDepartmentPhone: "08030738000",
                 deputy: "Mrs. Motolani Somoye"
             },
             {
                 name: "TUCK SHOP DEPARTMENT",
                 head: "Mrs Ayobami Lala",
+                gender: "f",
+                headOfDepartmentPhone: "08105412151",
                 deputy: "Mrs Tomisin Ogunjobi"
             },
             {
                 name: "CRECHE DEPARTMENT",
                 head: "Mrs. Funmi Omotoso",
+                gender: "f",
+                headOfDepartmentPhone: "07061571043",
                 deputy: "Miss Nkiruka Brown"
             },
             {
                 name: "FACILITY MANAGEMENT DEPARTMENT",
                 head: "Mr. Femi Patrick Kupoluyi",
+                gender: "m",
+                headOfDepartmentPhone: "08088917586",
                 deputy: "Mr Yinka Lawal"
             },
             {
                 name: "STAGE MANAGEMENT DEPARTMENT",
                 head: "Mr. Mayowa Aderogbin",
+                gender: "m",
+                headOfDepartmentPhone: "08056849502",
                 deputy: "Mr Gboyega Atanda"
             },
             {
                 name: "LOVE-IN-ACTION DEPARTMENT",
                 head: "Mr Seun Okeyanju",
+                gender: "m",
+                headOfDepartmentPhone: "08038333703",
                 deputy: "Miss Lolade Aderinto"
             },
             {
                 name: "WELFARE DEPARTMENT",
                 head: "Miss Doris Adebayo",
+                gender: "f",
+                headOfDepartmentPhone: "07060584030",
                 deputy: "Miss Kemi Kupoluyi"
             }
         ]
@@ -255,21 +295,29 @@ const directorateObject = [
     {
         name: "WORSHIP DIRECTORATE",
         head: "Pst. Shola Lawal",
+        headOfDirectoratePhone: "08067115436",
         deputy: "Mr Tomisin Kehinde",
+        gender: "m",
         departments: [
             {
                 name: "CHOR DEPARTMENT",
                 head: "Pst. Shola Lawal",
+                gender: "m",
+                headOfDepartmentPhone: "08067115436",
                 deputy: "Mr. Tomisin Kehinde"
             },
             {
                 name: "DRAMA DEPARTMENT",
                 head: "Mr. Olumide Jesumuyiwa ",
+                gender: "m",
+                headOfDepartmentPhone: "08162760891",
                 deputy: "Mr Micheal Ogunbo"
             },
             {
                 name: "DANCE DEPARTMENT",
                 head: "Mr. Malachi Alozie",
+                gender: "m",
+                headOfDepartmentPhone: "08166967976",
                 deputy: "Mr Babatunde Tiamiyu"
             }
         ]
@@ -277,36 +325,50 @@ const directorateObject = [
     {
         name: "OUTREACHES DIRECTORATE",
         head: "Min. Biola Dada",
+        headOfDirectoratePhone: "07038616699",
         deputy: "Mr. Sola Kolawole",
+        gender: "m",
         departments: [
             {
                 name: "MARKETING COMMUNICATIONS DEPARTMENT",
                 head: "Mr Sola Kolawole",
+                gender: "m",
+                headOfDepartmentPhone: "08032219727",
                 deputy: "Mr Wale Ogunjobi"
             },
             {
                 name: "PUBLICITY",
                 head: "Mr Kolade Adepoju",
+                gender: "m",
+                headOfDirectoratePhone: "07036294055",
                 deputy: "Mr Ojo Ebenezer"
             },
             {
                 name: "CAMPUS OUTREACHES DEPARTMENT",
                 head: "Mr. Olalekan Oluwatoba.",
+                gender: "m",
+                headOfDepartmentPhone: "08037237259",
                 deputy: "Mr. Tosin Ogunmade "
             },
             {
                 name: "SOCIAL MEDIA DEPARTMENT",
                 head: "Mrs Dayo Olufemi",
+                gender: "f",
+                headOfDepartmentPhone: "08055384847",
                 deputy: "Mr Ademola Adetona"
             },
             {
                 name: "DOME DEPARTMENT",
                 head: "Mrs Maryanne Onifade",
+                gender: "f",
+                headOfDepartmentPhone: "07034442330",
                 deputy: "Mrs Cecilia Olaleye"
             },
             {
                 name: "TRANSPORTATION AND LOGISTICS DEPARTMENT",
                 head: "Mr. Tade Okewole",
+                gender: "m",
+                headOfDepartmentPhone: "08038317289",
                 deputy: "Mr Jide Olaleye "
             }
         ]
@@ -314,36 +376,50 @@ const directorateObject = [
     {
         name: "EDIFICATION DIRECTORATE",
         head: "Min. Bolanle Dada",
+        headOfDirectoratePhone: "07035495135",
         deputy: "Mrs Tomi Fadoju",
+        gender: "m",
         departments: [
             {
                 name: "TABERNACLE OF DAVID DEPARTMENT",
                 head: "Mrs Bolanle Dada. ",
+                gender: "f",
+                headOfDepartmentPhone: "07035495135",
                 deputy: ""
             },
             {
                 name: "C.O.D Coordinator",
                 head: "Mrs Tomi Fadoju.",
+                gender: "f",
+                headOfDepartmentPhone: "08128921465",
                 deputy: "Mr Timileyin Agbeyo"
             },
             {
                 name: "INTERCESSORY DEPARTMENT",
                 head: "Mr Gboyega Hassan",
+                gender: "m",
+                headOfDepartmentPhone: "07056236378",
                 deputy: "Mr. Joseph Sanni"
             },
             {
                 name: "CAREER DEVELOPMENT DEPARTMENT",
                 head: "Miss Shola Orimoloye",
+                gender: "f",
+                headOfDepartmentPhone: "08160006061",
                 deputy: "Miss Princess Ubong"
             },
             {
                 name: "WOMEN FORUM DEPARTMENT",
                 head: "Mrs Ibukun Adedoyin",
+                gender: "f",
+                headOfDepartmentPhone: "08022611261",
                 deputy: "Mrs Bukola Obisesan"
             },
             {
                 name: "MEN FORUM DEPARTMENT",
                 head: "Mr. Daniel Olufemi",
+                gender: "m",
+                headOfDepartmentPhone: "07039601010",
                 deputy: "Kennedy Oboh"
             }
         ]
@@ -351,26 +427,36 @@ const directorateObject = [
     {
         name: "TECHNOLOGY DIRECTORATE",
         head: "Min. Gbenga Folarin",
+        headOfDirectoratePhone: "08089773815",
         deputy: "Mr. Ayodeji Gregory",
+        gender: "m",
         departments: [
             {
                 name: "VIDEO AND PHOTOGRAPHY DEPARTMENT",
                 head: "Mr Wale Akadiri",
+                gender: "m",
+                headOfDepartmentPhone: "08169416249",
                 deputy: "Mr Niran Adesanya"
             },
             {
                 name: "TECHNICAL DEPARTMENT",
-                head: "Mr  Fisayo Ola",
+                head: "Mr Fisayo Ola",
+                gender: "m",
+                headOfDepartmentPhone: "08034774796",
                 deputy: "Mr Omolade Akinbode"
             },
             {
                 name: "MULTIMEDIA DEPARTMENT",
                 head: "Mr Fadoju Olakunle",
+                gender: "m",
+                headOfDepartmentPhone: "07062182179",
                 deputy: "Mr Lasisi Damilare"
             },
             {
                 name: "MEDIA PRODUCTION DEPARTMENT",
                 head: "Mr Ayodeji Gregory",
+                gender: "m",
+                headOfDepartmentPhone: "08167225755",
                 deputy: "Mr Tiamiyu Adeyinka"
             }
         ]
@@ -378,16 +464,22 @@ const directorateObject = [
     {
         name: "SMALL GROUPS DIRECTORATE",
         head: "Min. Nifemi Oguntoye",
+        headOfDirectoratePhone: "08035181304",
         deputy: "Mr Dipo Salako",
+        gender: "m",
         departments: [
             {
                 name: "SMALL GROUPS DEPARTMENT",
                 head: "Mr Dipo Salako",
+                gender: "m",
+                headOfDepartmentPhone: "07033162200",
                 deputy: "Mrs Tunrayo Koyejo"
             },
             {
                 name: "LAGOS DEPARTMENT",
                 head: "Miss Toyosi Akinola",
+                gender: "f",
+                headOfDepartmentPhone: "08075436691",
                 deputy: "Mrs Rachael Salako"
             }
         ]
@@ -422,7 +514,8 @@ const findDepartmentLeader = (department) => {
             //console.log(dep[j].head)
             if (dep[j].name == department) {
                 //console.log(dep[j].head)
-                return dep[j].head;
+                localStorage.setItem("headOfDepartmentGender", dep[j].gender);
+                return dep[j].head + " (" + dep[j].headOfDepartmentPhone + ")";;
             }
         }
     }
@@ -432,7 +525,7 @@ const findDirectorateLeader = (directorate) => {
     for (let i = 0; i < directorateObject.length; i++) {
         const name = directorateObject[i].name;
         if (directorate == name) {
-            return directorateObject[i].head;
+            return directorateObject[i].head + " (" + directorateObject[i].headOfDirectoratePhone + ")";
         }
     }
 }
